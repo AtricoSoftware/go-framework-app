af1a33e319df67c7de8c6ba8d2bfbdeefa643cff
Rob <rob.rimmer@mclaren.com>
2021-06-02 10:22:56 +0100

    Upgrade viperEx Fix resources in build.sh

--- build.sh
+++ build.sh
@@ -70,6 +70,8 @@ LDFLAGS=$LDFLAGS" -X '$MODULE/pkg.Version=$VERSION'"
 LDFLAGS=$LDFLAGS" -X '$MODULE/pkg.BuildDetails=$DETAILS'"
 # SECTION-END
 
+go run ./create_resources.go
+
 # SECTION-START: Build
 mkdir -p $BUILD_DIR
 for GOARCH in ${ARCH[@]}; do
