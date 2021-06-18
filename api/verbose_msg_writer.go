e481f733931bf06bfe8c46b7807d03a18897169b
Rob <rob.rimmer@mclaren.com>
2021-06-18 17:33:37 +0100

    Improve verbose print

--- api/verbose_msg_writer.go
+++ api/verbose_msg_writer.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"fmt"
+	"io"
 )
 
 var VerboseFlag bool
@@ -15,6 +16,22 @@ func VerbosePrintln(a ...interface{}) (n int, err error) {
 }
 
 func VerbosePrintf(format string, a ...interface{}) (n int, err error) {
+	if VerboseFlag {
+		return fmt.Printf(format, a...)
+	} else {
+		return 0, nil
+	}
+}
+
+func VerboseFprintln(w io.Writer, a ...interface{}) (n int, err error) {
+	if VerboseFlag {
+		return fmt.Println(a...)
+	} else {
+		return 0, nil
+	}
+}
+
+func VerboseFprintf(w io.Writer, format string, a ...interface{}) (n int, err error) {
 	if VerboseFlag {
 		return fmt.Printf(format, a...)
 	} else {
--- resources/files/api/_verbose_msg_writer.go
+++ resources/files/api/_verbose_msg_writer.go
@@ -2,21 +2,31 @@ package api
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
 var VerboseFlag bool
 
 func VerbosePrintln(a ...interface{}) (n int, err error) {
+	return VerboseFprintln(os.Stdout, a...)
+}
+
+func VerbosePrintf(format string, a ...interface{}) (n int, err error) {
+	return VerboseFprintf(os.Stdout, format, a...)
+}
+
+func VerboseFprintln(w io.Writer, a ...interface{}) (n int, err error) {
 	if VerboseFlag {
-		return fmt.Println(a...)
+		return fmt.Fprintln(w, a...)
 	} else {
 		return 0, nil
 	}
 }
 
-func VerbosePrintf(format string, a ...interface{}) (n int, err error) {
+func VerboseFprintf(w io.Writer, format string, a ...interface{}) (n int, err error) {
 	if VerboseFlag {
-		return fmt.Printf(format, a...)
+		return fmt.Fprintf(w, format, a...)
 	} else {
 		return 0, nil
 	}
